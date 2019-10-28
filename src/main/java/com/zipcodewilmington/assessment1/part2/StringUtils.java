eaaf81499a7e21383f3f56f4a5256bde29915e05
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-10-28 16:58:52 -0400

    git

--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -10,47 +10,50 @@ public class StringUtils {
      * @return an array of strings, representative of each word in the sentence
      * given a string containing words delimited by spaces, representative of a sentence, return an array of strings, each element representative of a respective word in the sentence
      */
-    public static String[] getWords(String sentence) {
-        return null;
+    public static String getWords(String sentence) {
+
+        String strArray[] = sentence.split(", ");
+        return strArray[Integer.parseInt(sentence)];
+
+
+        /**
+         * @param sentence a string containing words delimited by spaces, representative of a sentence
+         * @return the first word in the specified sentence
+         * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
+         */
+        public static String getFirstWord (String sentence){
+
+            String w = "Quick";
+            return w;
+        }
+
+        /**
+         * @param sentence a string containing words delimited by spaces, representative of a sentence
+         * @return the first word in the specified sentence, with identical contents in reverse order
+         * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
+         */
+        public static String reverseFirstWord (String sentence){
+            return null;
+        }
+
+        /**
+         * @param sentence a string containing words delimited by spaces, representative of a sentence
+         * @return the first word in the specified sentence, with identical contents in reverse order and the first character capitalized
+         * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
+         */
+        public static String reverseFirstWordThenCamelCase (String sentence){
+            return null;
+        }
+
+
+        /**
+         * @param str string input from client
+         * @param index the index of the character to be removed from `str`
+         * @return string with identical contents, excluding the character at the specified index
+         * given a string and index, return an identical string excluding the character at the specified index
+         */
+        public static String removeCharacterAtIndex (String str,int index){
+            return null;
+        }
     }
-
-
-    /**
-     * @param sentence a string containing words delimited by spaces, representative of a sentence
-     * @return the first word in the specified sentence
-     * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
-     */
-    public static String getFirstWord(String sentence) {
-        return null;
-    }
-
-    /**
-     * @param sentence a string containing words delimited by spaces, representative of a sentence
-     * @return the first word in the specified sentence, with identical contents in reverse order
-     * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
-     */
-    public static String reverseFirstWord(String sentence) {
-        return null;
-    }
-
-    /**
-     * @param sentence a string containing words delimited by spaces, representative of a sentence
-     * @return the first word in the specified sentence, with identical contents in reverse order and the first character capitalized
-     * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
-     */
-    public static String reverseFirstWordThenCamelCase(String sentence) {
-        return null;
-    }
-
-
-    /**
-     * @param str string input from client
-     * @param index the index of the character to be removed from `str`
-     * @return string with identical contents, excluding the character at the specified index
-     * given a string and index, return an identical string excluding the character at the specified index
-     */
-    public static String removeCharacterAtIndex(String str, int index) {
-        return null;
-    }
-
 }
--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,12 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
+
+
+
+
+
+
         return -1;
     }
 }
